bb1bcda3a522b4896fab6c3bb7a8bd2d1c13aaba
David Dray <draydr17@gcc.edu>
2020-03-28 17:02:14 -0400

    sufficiently commented program

--- Matrix.cpp
+++ Matrix.cpp
@@ -32,7 +32,7 @@ int main() {
     double* mat2;  //seciond matrix (3 x 7)
     double* mat3;  //matrix resulting from mat1*mat2 (250000 x 7)
     double incrVal; //value to increment matrix elements by
-    int x = 0; //value multiplied by incrVal -increments for every element
+    int x = 0; //increments for every element (multiplied by incrVal)
     double startTime, stopTime; //keeps track of time multiplication took
     double serialTime, pTime;   //stores serial and parallel times
     FILE* fp;   //file variable for writing to a file
@@ -85,7 +85,7 @@ int main() {
 
     
     //inform user of data outputting to file
-    printf("Printing results to Dray.csv in current directory\n");
+    printf("\n\nPrinting results to Dray.csv in current directory\n");
 
     /* open the file for writing*/
     fp = fopen("Dray.csv", "w");
@@ -117,50 +117,94 @@ int main() {
 
 }
 
+/************************************************************************
+* Function:    multiply (serial)
+
+* Purpose:     take in 2 matrices along with their sizes and multiply the
+    the first by the 2nd, timing how long it takes
+
+* Input args:  takes in the number of rows and columns of matrix 1,
+    the number of rows and columns in matrix 2, the two 2D arrays 
+    (our matrices), and pointers to the timing variables
+
+* sources:     This multiplication method was manipulated but based off
+    of Aditya Ranjan's algorithm (found in the link provided in the
+    project description).
+************************************************************************/
 double * multiply(int mat1Row, int mat1Col, double *mat1,
     int mat2Row, int mat2Col, double *mat2,
     double* startTime, double* stopTime) {
-    int x, r, c;
+    int x, r, c;    //loop variables
+    //product of the two matrices' multiplication
     double * res = (double*)malloc(mat1Row * mat2Col * sizeof(double));
-    *startTime = omp_get_wtime();
 
+    *startTime = omp_get_wtime();       //start timing
+
+    //loop through the columns of matrix 2 and the rows of matrix 1
     for (c = 0; c < mat2Col; c++) {
         for (r = 0; r < mat1Row; r++) {
-            *(res+ r* mat2Col+c) = 0;
+            *(res+ r* mat2Col+c) = 0;   //set product's i,j element to 0
+
+            //loop to get dot products for each i,j element
+            //notice summing each mulitplication into product matrix
             for (x = 0; x < mat1Col; x++) {
                 *(res + r * mat2Col + c) += *(mat1 + r * mat1Col + x) *
                     *(mat2 + x * mat2Col + c);
             }
         }
     }
-    *stopTime = omp_get_wtime();
+    *stopTime = omp_get_wtime();        //stop timing
 
-    return res;
+    return res;     //return the resulting 2d array (matrix)
 }
 
+/************************************************************************
+* Function:    parallelMultiply
+
+* Purpose:     take in 2 matrices along with their sizes and multiply the
+    the first by the 2nd using cyclical parallel computing. It also times
+    how long it takes.
+
+* Input args:  takes in the number of rows and columns of matrix 1,
+    the number of rows and columns in matrix 2, the two 2D arrays
+    (our matrices), and pointers to the timing variables
+
+* sources:     This multiplication method was manipulated but based off
+    of Aditya Ranjan's algorithm (found in the link provided in the
+    project description).
+************************************************************************/
 double* parallelMultiply(int mat1Row, int mat1Col, double* mat1,
     int mat2Row, int mat2Col, double* mat2,
     double * startTime, double * stopTime) {
 
+    //product of the two matrices' multiplication
     double* res = (double*)malloc(mat1Row * mat2Col * sizeof(double));
 
-    *startTime = omp_get_wtime();
-#pragma omp parallel num_threads(2)
+    *startTime = omp_get_wtime();       //start timing
+
+#pragma omp parallel num_threads(2) //give me 2 threads 
     {
         int x, r, c;    //loop control variables for each thread
         int threadNum = omp_get_num_threads();  //how many?
         int threadId = omp_get_thread_num();    //who am I?
+        
+        //loop through the rows of the large matrix 
+        //(want to do this as infrequent as possible)
         for (r = threadId; r < mat1Row; r += threadNum) {
+            //loop through the 2nd matrix's columns
             for (c = 0; c < mat2Col; c++) {
-                *(res + r * mat2Col + c) = 0;
+                *(res + r * mat2Col + c) = 0;   //set product's i,j to 0
+                
+                //loop to get dot products for each i,j element
+                //notice summing each mulitplication into product matrix
                 for (x = 0; x < mat1Col; x++) {
-                    *(res + r * mat2Col + c) += *(mat1 + r * mat1Col + x) *
+                    *(res + r * mat2Col + c) += *(mat1 +r * mat1Col + x)*
                         *(mat2 + x * mat2Col + c);
                 }
             }
         }
     }
-    *stopTime = omp_get_wtime();
+    *stopTime = omp_get_wtime();        //stop timing
 
-    return res;
+    return res; //return the result matrix
 }
