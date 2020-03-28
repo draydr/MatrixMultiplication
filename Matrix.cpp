9863e60545f3e9bf786005f76cf1fcf17f078ea5
David Dray <draydr17@gcc.edu>
2020-03-27 21:35:25 -0400

    added parallel method and attempted parallelization

--- Matrix.cpp
+++ Matrix.cpp
@@ -16,23 +16,25 @@
 #include<omp.h>
 
 // method stubs
-double * multiply(int m1, int m2, double *mat1,
+double* multiply(int m1, int m2, double *mat1,
     int n1, int n2, double *mat2);
-
+double* parallelMultiply(int mat1Row, int mat1Col, double* mat1,
+    int mat2Row, int mat2Col, double* mat2);
 
 int main() {
     //variable dictionary:
     //number of rows and columns for the 2 matrices
-    const int row1 = 25, col1 = 3, row2 = 3, col2 = 7;
+    const int row1 = 250000, col1 = 3, row2 = 3, col2 = 7;
     int r,c;  //loop variables
     double* mat1;  //first matrix (250000 x 3)
     double* mat2;  //seciond matrix (3 x 7)
     double* mat3;  //matrix resulting from mat1*mat2 (250000 x 7)
     double incrVal; //value to increment matrix elements by
-    int x = 0;  //value multiplied by incrVal - increments for every element
+    int x = 0; //value multiplied by incrVal -increments for every element
     //second matrix (3 x 7)
     double startTime, stopTime; //keeps track of time multiplication took
-        
+    double serialTime, pTime;   //stores serial and parallel times
+    
     //standard initial output
     printf("David Dray ~ COMP 233 B ~ Spring 2020\n");
     printf("Matrix - Multiply Matrices together and "
@@ -47,15 +49,6 @@ int main() {
             *(mat1 + r * col1 + c) = incrVal *x++;
         }
     }
-
-    printf("matrix1:\n");
-    for (r = 0; r < row1; r++) {
-        for (c = 0; c < col1; c++) {
-            printf("%f ", *(mat1 + r * col1 + c));
-        }
-        printf("\n");
-    }
-
    
     mat2 = (double*)malloc(row1 * col1 * sizeof(double));
     //initialize element's values
@@ -74,25 +67,20 @@ int main() {
             *(mat2 + r * col2 + (c+3)) = c;
         }
     }
-    
-   
-    printf("matrix2:\n");
-    for (r = 0; r < row2; r++) {
-        for (c = 0; c < col2; c++) {
-            printf("%f ", *(mat2 + r * col2 + c));
-        }
-        printf("\n");
-    }
-
-
 
     mat3 = (double*)malloc(row1 * col2 * sizeof(double));
 
     startTime = omp_get_wtime();
-    mat3 = multiply(row1, col1, mat1, row2, col2, mat2);
+    multiply(row1, col1, mat1, row2, col2, mat2);
     stopTime = omp_get_wtime();
+    serialTime = stopTime - startTime;
 
-    printf("\n\nmatrix3:\n");
+    startTime = omp_get_wtime();
+    mat3 = parallelMultiply(row1, col1, mat1, row2, col2, mat2);
+    stopTime = omp_get_wtime();
+    pTime = stopTime - startTime;
+
+    /*printf("\n\nmatrix3:\n");
     for ( r = 0; r < row1; r++)
     {
         for (c = 0; c < col2; c++)
@@ -100,8 +88,10 @@ int main() {
             printf("%f ", *(mat3 + r * col2 + c));
         }
         printf("\n");
-    }
-    printf("Serial multiplication time: %f\n", stopTime - startTime);
+    }*/
+
+    printf("Serial multiplication time: %f\n", serialTime);
+    printf("Parallel multiplication time: %f\n", pTime);
     free(mat1);
     free(mat3);
 
@@ -117,7 +107,6 @@ double * multiply(int mat1Row, int mat1Col, double *mat1,
     double * res = (double*)malloc(mat1Row * mat2Col * sizeof(double));
 
     for (c = 0; c < mat2Col; c++) {
-//#pragma omp parallel 
         for (r = 0; r < mat1Row; r++) {
             *(res+ r* mat2Col+c) = 0;
             for (x = 0; x < mat1Col; x++) {
@@ -126,6 +115,27 @@ double * multiply(int mat1Row, int mat1Col, double *mat1,
             }
         }
     }
+    return res;
+}
 
+double* parallelMultiply(int mat1Row, int mat1Col, double* mat1,
+    int mat2Row, int mat2Col, double* mat2) {
+    int x, r, c;
+    double* res = (double*)malloc(mat1Row * mat2Col * sizeof(double));
+
+#pragma omp parallel num_threads(2)
+    {
+        int threadNum = omp_get_num_threads();  //how many?
+        int threadId = omp_get_thread_num();    //who am I?
+        for (c = 0; c < mat2Col; c++) {
+            for (r = threadId; r < mat1Row; r+=threadNum) {
+                *(res + r * mat2Col + c) = 0;
+                for (x = 0; x < mat1Col; x++) {
+                    *(res + r * mat2Col + c) += *(mat1 + r * mat1Col + x) *
+                        *(mat2 + x * mat2Col + c);
+                }
+            }
+        }
+    }
     return res;
 }
